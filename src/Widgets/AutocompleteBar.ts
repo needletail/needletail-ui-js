d1e0f273ae1ca1ea24b915db391c68f0751c318a
Esdert Folkers <esdert@creativeorange.nl>
2021-02-27 10:30:00 +0100

    v3.0.71

--- src/Widgets/AutocompleteBar.ts
+++ src/Widgets/AutocompleteBar.ts
@@ -524,7 +524,7 @@ export class AutocompleteBar extends Widget {
                     let bucket_name: string = (r.bucket) ? r.bucket.toString() : '';
                     let mapped = {
                         ...r.record,
-                        bucket: (this.bucketMapping[bucket_name]) ? this.bucketMapping[bucket_name] : '',
+                        bucket: (bucket_name !== '' && this.bucketMapping[bucket_name]) ? this.bucketMapping[bucket_name] : '',
                         value: {},
                         raw: {}
                     }
