d2ec8f7a2468a8e60fe14803d5edf5ef45da4fa5
Edsardio <e.k.j.folkers@live.nl>
2022-03-30 16:10:14 +0200

    v3.1.13

--- src/Needletail.ts
+++ src/Needletail.ts
@@ -2,3 +2,5 @@ export * from './Client';
 export * from './Imports/Aggregations';
 export * from './Imports/Widgets';
 export * from './Helpers/Events';
+
+export * from '@needletail/js/src/Helpers/Config';
