b27ae890ab36cd15f9c645dcbb758c93eb89dba9
Esdert Folkers <e.k.j.folkers@live.nl>
2023-10-26 14:55:23 +0200

    V3.4.7

--- src/Needletail.ts
+++ src/Needletail.ts
@@ -2,4 +2,5 @@ export * from './Client';
 export * from './Imports/Aggregations';
 export * from './Imports/Widgets';
 export * from './Helpers/Events';
+export * from './Helpers/URIHelper';
 import './Css/needletail.scss';
